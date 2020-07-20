75d8d06f9e7631654f36584e3b6b77c6890c451c
Ahmed Hilles <57227022+Ahmed116@users.noreply.github.com>
2020-07-20 17:33:39 +0300

    heroku

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
-import React from "react";
+import React from 'react';
 //import SearchBooks from "./search.js";
-import "./App.css";
-import Registration from "./registration.js";
-import Login from "./login.js";
+import './App.css';
+import Registration from './registration.js';
+import Login from './login.js';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
-} from "react-router-dom";
+} from 'react-router-dom';
 // import App2 from './App2'
- class App extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      isAuthenticated: true,
     };
   }
   setUserAuth = (value) => this.setState({ isAuthenticated: true });
   render() {
     return (
-      <div className="app">
-       
+      <div className='app'>
         <Router>
           <Switch>
-            <Route path="/auth/login">
+            <Route path='/auth/login'>
               <Login setUserAuth={this.setUserAuth} />
             </Route>
-            <Route exact path="/auth/reg">
+            <Route exact path='/auth/reg'>
               <Registration setUserAuth={this.setUserAuth} />
             </Route>
-            <PrivateRoute isAuthenticated={this.state.isAuthenticated} path="/">
+            <PrivateRoute isAuthenticated={this.state.isAuthenticated} path='/'>
               {/* <App2 /> */}
               <div>
                 <h1>helloooo world</h1>
@@ -52,7 +51,7 @@ function PrivateRoute({ children, isAuthenticated, ...rest }) {
         ) : (
           <Redirect
             to={{
-              pathname: "/auth/login",
+              pathname: '/auth/login',
               state: { from: location },
             }}
           />
