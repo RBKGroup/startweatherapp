f4102f6ad352b08c84b1d3918c62071aa9193601
NeveenEl-Khozondar <neveen.khozondar11@gmail.com>
2020-07-22 13:49:37 +0300

    aditcss

---
+++ src/logout.js
@@ -0,0 +1,29 @@
+// import React from 'react';
+// import axios from 'axios';
+
+// import { Link, withRouter } from 'react-router-dom';
+// class Logout extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     return (
+//       <div className='inner-container'>
+//         <div class='h1'>
+//           <h1>Weather App</h1>
+//         </div>
+//           <p>
+//             you have alredy account !{' '}
+//             <Link to='/auth/login' class='link'>
+//               {' '}
+//               login now
+//             </Link>
+//           </p>
+     
+//       </div>
+//     );
+//   }
+// }
+
+// export default withRouter(Logout);
